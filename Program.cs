49111f5733d6e698dac0a8b8ddb541868ae9137f
Balázs <Balzs@DESKTOP-ID885J2>
2024-03-15 11:38:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace oszthatoe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérlek adj meg egy számot: ");
+             double szam = double.Parse(Console.ReadLine());
+
+            if (szam % 3 == 0 & szam % 4 == 0 & szam % 9 == 0){
+                Console.WriteLine("A szám osztható 3-mal,4-gyel és 9-cel");
+            }
+            else if (szam % 3 == 0 & szam % 4 == 0)
+            {
+                Console.WriteLine("A szám osztható 3-mal és 4-gyel");
+            }
+            else if (szam % 3 == 0)
+            {
+                Console.WriteLine("A szám osztható 3-mal");
+            }
+            else if (szam % 4 == 0 & szam % 9 == 0)
+            {
+                Console.WriteLine("A szám osztható 4-gyel és 9-cel");
+            }
+            else if (szam % 9 == 0 & szam % 3 == 0)
+            {
+                Console.WriteLine("A szám osztható 3-mal és 9-cel");
+            }
+            else if (szam % 4 == 0)
+            {
+                Console.WriteLine("A szám osztható 4-gyel");
+            }
+            else if (szam % 9 == 0)
+            {
+                Console.WriteLine("A szám osztható 9-cel");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 3-mal, 4-gyel sem 9-cel");
+            }
+        }
+    }
+}
